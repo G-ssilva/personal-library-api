e033ce7c32bf7e4f7b7fa15cb5378f061c5c5f9b
Guilherme Silva <guissilva_@outlook.com>
2024-01-08 19:30:40 -0300

    Initial commit

---
+++ src/main/java/br/com/gssilva/personallibraryapi/PersonalLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gssilva.personallibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gssilva/personallibraryapi/PersonalLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gssilva.personallibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
