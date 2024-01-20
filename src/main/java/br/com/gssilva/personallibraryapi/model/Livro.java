5aa47057b3937aad216ed24c1bd1458d837991ad
Guilherme Silva <guissilva_@outlook.com>
2024-01-19 21:08:31 -0300

    FIX - definindo colunas do Livro como NOTNULL.

--- src/main/java/br/com/gssilva/personallibraryapi/model/Livro.java
+++ src/main/java/br/com/gssilva/personallibraryapi/model/Livro.java
@@ -1,6 +1,7 @@
 package br.com.gssilva.personallibraryapi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,26 +19,33 @@ public class Livro {
     private long id;
 
     @Getter @Setter
+    @NotNull
     private String titulo;
 
     @Getter @Setter
+    @NotNull
     private String autor;
 
     @Getter @Setter
+    @NotNull
     @Column(name = "numero_paginas")
     private long numeroPaginas;
 
     @Getter @Setter
+    @NotNull
     private String idioma;
 
     @Getter @Setter
+    @NotNull
     private String editora;
 
     @Getter @Setter
+    @NotNull
     @Column(name = "data_publicacao")
     private LocalDate dataPublicacao;
 
     @Getter @Setter
+    @NotNull
     @JoinColumn(name = "usuario_id")
     @ManyToOne
     private Usuario usuarioId;
