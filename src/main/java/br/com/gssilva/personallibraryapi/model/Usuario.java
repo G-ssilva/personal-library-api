ee2827897c83b2e8de01a39e470d3a5843541b8a
Guilherme Silva <guissilva_@outlook.com>
2024-01-19 21:11:40 -0300

    FIX - definindo colunas do Usuario como NOTNULL.

--- src/main/java/br/com/gssilva/personallibraryapi/model/Usuario.java
+++ src/main/java/br/com/gssilva/personallibraryapi/model/Usuario.java
@@ -1,6 +1,7 @@
 package br.com.gssilva.personallibraryapi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,13 +17,16 @@ public class Usuario {
     private long id;
 
     @Getter @Setter
+    @NotNull
     @Column(unique = true)
     private String login;
 
     @Getter @Setter
+    @NotNull
     private String senha;
 
     @Getter @Setter
+    @NotNull
     @JoinColumn(name = "perfil_id")
     @ManyToOne
     private Perfil perfilId;
